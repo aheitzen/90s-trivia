9720f4b09160a75bf48e0594cb17a23bf212b8d1
aheitzen <alexaheitzenrader@yahoo.com>
2017-04-21 13:48:56 -0700

    added gif animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log('I work');
+var index = 0;
+
+var giphyArray = [ 
+	{
+		url: "pasta.gif",
+		delay: 2000
+	},
+	{
+		url: "spice.gif",
+		delay: 2000
+	},
+	{
+		url: "toma.gif",
+		delay: 1600
+	},
+	{
+		url: "vodka.gif",
+		delay: 1000
+	},
+	{
+		url: "",
+		delay: 0
+	}
+];
+
+function gifAnimation() {
+	$('.animationClass').css('background-image', 'url("./gifs/'+ giphyArray[index].url + '")');
+	if(index < giphyArray.length -1) {
+		var delay = giphyArray[index].delay;
+		index++;
+		setTimeout(gifAnimation, delay);
+	}
+}
+gifAnimation();
+
+
